682620926065b1fd21728c87e1145e6542b43003
Thibault LE GUENNEC <thibault.LE-GUENNEC@etu.univ-amu.fr>
2025-04-02 17:25:47 +0200

    update

---
+++ src/main/java/fr/univamu/iut/projet/filter/CorsFilter.java
@@ -0,0 +1,30 @@
+package fr.univamu.iut.projet.filter; // Choisis un package approprié
+
+import jakarta.ws.rs.container.*;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.Provider;
+
+import java.io.IOException;
+
+@Provider
+@PreMatching
+public class CorsFilter implements ContainerRequestFilter, ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        if (requestContext.getRequest().getMethod().equals("OPTIONS")) {
+            Response.ResponseBuilder builder = Response.ok()
+                    .header("Access-Control-Allow-Origin", "*") // ATTENTION: Utilise une origine spécifique en production
+                    .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+                    .header("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept, X-Requested-With");
+
+            requestContext.abortWith(builder.build());
+        }
+    }
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*"); // ATTENTION: Doit correspondre à celui de la requête OPTIONS ou être plus spécifique
+
+    }
+}
