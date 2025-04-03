a2c1abba9abfd2c22e872fd841fdf7fdc85161b1
LEGUENNEC-Thibault-23001768 <146089304+LEGUENNEC-Thibault-23001768@users.noreply.github.com>
2025-04-03 20:41:40 +0200

    Update CorsFilter.java

--- src/main/java/fr/univamu/iut/projet/filter/CorsFilter.java
+++ src/main/java/fr/univamu/iut/projet/filter/CorsFilter.java
@@ -12,19 +12,22 @@ public class CorsFilter implements ContainerRequestFilter, ContainerResponseFilt
 
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
-        if (requestContext.getRequest().getMethod().equals("OPTIONS")) {
-            Response.ResponseBuilder builder = Response.ok()
+        if (requestContext.getRequest().getMethod().equalsIgnoreCase("OPTIONS")) {
+            requestContext.abortWith(Response.ok()
                     .header("Access-Control-Allow-Origin", "*")
                     .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
-                    .header("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept, X-Requested-With");
-
-            requestContext.abortWith(builder.build());
+                    .header("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept, X-Requested-With")
+                    .header("Access-Control-Allow-Credentials", "true")
+                    .header("Access-Control-Max-Age", "86400") // Cache preflight response for 24hrs
+                    .build());
         }
     }
 
     @Override
-    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
-
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
+        responseContext.getHeaders().putSingle("Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().putSingle("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        responseContext.getHeaders().putSingle("Access-Control-Allow-Credentials", "true");
+        responseContext.getHeaders().putSingle("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept, X-Requested-With");
     }
 }
